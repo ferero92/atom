e6fc0f4824ec61f574d486537945b03129bcc54e
ferero92 <ferero92@hotmail.com>
2016-11-19 17:52:23 +0100

    19/11/16

--- Cliente/AJAX/encuesta/code.js
+++ Cliente/AJAX/encuesta/code.js
@@ -1,11 +1,4 @@
-function load() {
-
-  if(window.XMLHttpRequest) {
-    ajax = new XMLHttpRequest();
-  }
-  else if (window.ActiveXObject) {
-    ajax = new ActiveXObject('Microsoft.XMLHttpRequest');
-  }
+function vote() {
 
   var lan = document.getElementsByName('lan');
   var vote;
@@ -14,21 +7,66 @@ function load() {
     if(lan[i].checked)
       vote = lan[i].value;
   }
+  load('http://localhost/encuesta/vote.php?vote=PHP');
+}
+
+function load(url) {
+
+  if(window.XMLHttpRequest) {
+    ajax = new XMLHttpRequest();
+  }
+  else if (window.ActiveXObject) {
+    ajax = new ActiveXObject('Microsoft.XMLHttpRequest');
+  }
+
+  ajax.open('GET', url, true);
+  ajax.send(null);
 
   ajax.onreadystatechange = function (){
-    document.getElementsByTagName('fieldset')[0].innerHTML = '<img src="load.gif" style="width: 30px; height: 30px;"/>';
     if(ajax.readyState == 4){
       if(ajax.status == 200){
         stadist();
       }
     }
   };
-
-  ajax.open('GET', 'http://localhost/encuesta/voto.php?vote='+vote, true);
-  ajax.send(null);
 }
 
 function stadist() {
 
-  var 
+  alert('ok');
+  alert(ajax.responseText);
 }
+
+// function load() {
+//
+//   if(window.XMLHttpRequest) {
+//     ajax = new XMLHttpRequest();
+//   }
+//   else if (window.ActiveXObject) {
+//     ajax = new ActiveXObject('Microsoft.XMLHttpRequest');
+//   }
+//
+//   var lan = document.getElementsByName('lan');
+//   var vote;
+//
+//   for (var i = 0; i < lan.length; i++) {
+//     if(lan[i].checked)
+//       vote = lan[i].value;
+//   }
+//
+//   ajax.onreadystatechange = read;
+//
+//   ajax.open('GET', 'http://localhost/encuesta/vote.php?vote='+vote, true);
+//   ajax.send(null);
+//
+//   document.getElementsByTagName('fieldset')[0].innerHTML = '<img src="load.gif" style="width: 50px; height: 50px;"/>';
+// }
+
+// function read(){
+//
+//   if(ajax.readyState == 4){
+//     if(ajax.status == 200){
+//       stadist();
+//     }
+//   }
+// }
--- Cliente/AJAX/encuesta/vote.php
+++ Cliente/AJAX/encuesta/vote.php
@@ -6,5 +6,10 @@
   $query = "UPDATE voto SET value = value +1 WHERE name = '" . $vote . "'";
   mysqli_query($conn, $query);
 
-  
+  $votes = "SELECT value FROM voto";
+  $result = mysqli_query($conn, $votes);
+
+  while ($row = mysqli_fetch_array($result)) {
+    echo $row["value"] . ",";
+  }
 ?>
